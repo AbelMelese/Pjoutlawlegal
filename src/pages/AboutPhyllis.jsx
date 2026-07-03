3383fc222aa36233ee56f09584324e1c108117cb
Abel Melese <AbelMelese329@gmail.com>
2026-07-03 20:53:27 +0300

    latest image closer

--- src/pages/AboutPhyllis.jsx
+++ src/pages/AboutPhyllis.jsx
@@ -74,7 +74,7 @@ const AboutPhyllis = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid md:grid-cols-2 gap-12 items-center">
             {/* Portrait on the left */}
-            <div className="flex justify-center md:justify-start">
+            <div className="flex justify-center md:justify-end">
               <div className="relative max-w-[50%]">
                 <div className="absolute -inset-4 bg-gradient-to-tr from-[#1E3A5F]/30 to-[#2A4F7A]/30 blur-xl"></div>
                 <div className="framed-photo-container relative">
